1500f192a6940b5f68a12eec51404c0bd8282839
ag3823 <alex@gocreations.net>
2021-01-09 18:18:29 -0800

    "added layout.html and bootstrap"

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.render('index.html');
 });
 
 app.listen(5000, function() {
