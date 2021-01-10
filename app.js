8f2a8d1b9dc0d91a770250ff401a3d42de5a0e00
Justin Aujla <justinaujla@hotmail.com>
2021-01-10 03:03:42 -0800

    More complex messages
    Messages are now objects with more data! No more stinky old strings for us

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const admin = require('firebase-admin')
+const serviceAccount = require('./ServiceAccountKey.json')
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
+
+const db = admin.firestore();
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/views/index.html');
@@ -13,15 +21,25 @@ app.get('/attend', function (req, res) {
 
 io.on('connection', (socket) => {
     console.log('a user connected');
-    socket.join('roomCode') //replace later with socket on method?
+
+    socket.join('roomCode')
+
+    // socket.on('setRoomCode', (data) => {
+    //     const destination = "/attend";
+    //     console.log('User Entered Room Code:' + data);
+    //     socket.join('roomCode');
+    //     socket.emit('redirect', destination);
+    // })
 
     socket.on('chat message', (data) => {
         console.log('user message:' + data);
+        // var data = JSON.parse(data)
+        // db.collection('messages')
         io.to('roomCode').emit('store message', data);
     })
 
 })
 
-http.listen(5000, function() {
+http.listen(5000, "0.0.0.0" ,function() {
     console.log("on port *:5000")
 })
