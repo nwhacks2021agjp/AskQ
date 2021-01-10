236761c77a81b4e5a705a02e1e89e24f3f1a7e67
ag3823 <alex@gocreations.net>
2021-01-09 18:29:05 -0800

    "changed templates to views and added new app.js code"

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-var express = require('express');
-var app = express();
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-app.get('/', function (req, res) {
-    res.render('index.html');
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
 });
 
-app.listen(5000, function() {
-    console.log("Example app listening on port 5000")
+io.on('connection', (socket) => {
+    console.log('New user connected')
+    // socket.username = "Anon"
+    // socket.on('change_username', (data) => {
+    //     socket.username = data.username
+    // })
+});
+
+http.listen(8888, () => {
+    console.log('listening on *:8888')
 })
